a82cc2778514009083120f25fc0229e6caa1a343
patcurry <currypat1985@gmail.com>
2018-02-20 17:10:44 +0100

    using create-react-app with css grid to mock up page layout... actually to make a page layout

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+          <div className="container">
+            <div className="header">HEADER</div>
+            <div className="main">
+              <div className="menu">MENU</div>
+              <div className="content">CONTENT</div>
+            </div>
+            <div className="footer">FOOTER</div>
+          </div>
+        </div>
+    );
+};
+
+export default App;
