0e1921bf8c015ce4f487c8b60741c9f4841a5dd5
patcurry <currypat1985@gmail.com>
2018-02-20 17:17:12 +0100

    oh yeah

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
 
+// This is great!
+// I can just write my html here, and let it sit and wait to be populated with data
+// and work on the styling and layout and stuff.
 const App = () => {
     return (
         <div className="App">
           <div className="container">
             <div className="header">HEADER</div>
             <div className="main">
-              <div className="menu">MENU</div>
+              <div className="menu">
+                <div>MENU</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+              </div>
               <div className="content">CONTENT</div>
             </div>
             <div className="footer">FOOTER</div>
